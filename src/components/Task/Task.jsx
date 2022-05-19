5695b9a352f2905227e3709dcfd522ed122b26c5
DiegoBelPe <dbeltranpereira@gmail.com>
2022-05-19 12:57:28 -0500

    solution bug tailwind.css

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -10,14 +10,17 @@ function Task() {
   }, []);
 
   return (
-    <div>
-      <h1>Task</h1>
+    <div className="container mx-auto mt-5">
+      <h1 className="bg-red-900">Task</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>{task.title}</li>
         ))}
       </ul>
-      <button className="btn-blue" type="button">Add Task</button>
+      <button className="btn btn-blue btn-blue:hover" type="button">Add Task</button>
     </div>
   );
 }
