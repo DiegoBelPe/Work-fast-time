92ba62c051c23b4a62e1c000775d90c1c91910ec
DiegoBelPe <dbeltranpereira@gmail.com>
2022-05-22 20:37:32 -0500

    page dashUser ok

---
+++ src/components/ButtonReport/ButtonReport.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import image from '../../assets/images';
+
+function ButtonReport() {
+  return (
+    <div className="homeStart mt-24">
+      <div>
+        <h2 className="text-center font-bold text-4xl text-white">Disfruta de nuestros servicios </h2>
+      </div>
+      <section className="py-20 px-20  grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 lg:h-screen lg:pt-32">
+        <div className="w-auto h-auto md:h-2/4  lg:h-2/4 rounded border border-solid shadow-lg flex flex-col items-center bg-blue-400">
+          <div>
+            <img className="w-40 h-40 " src={image.img3} alt="" />
+          </div>
+          <div className="block text-center text-white font-semibold text-lg">
+            ✅ Elabora informes de tu trabajo a la medida
+            de
+            tus necesidades y obten un archivo pdf con los resultados.
+          </div>
+        </div>
+        <div className="w-auto h-auto md:h-2/4  lg:h-2/4 rounded border border-solid shadow-lg flex flex-col items-center bg-blue-400">
+          <div>
+            <img className="w-40 h-40 " src={image.img4} alt="" />
+          </div>
+          <div className="block text-center text-white font-semibold text-lg">
+            ✅ Lleva un control de tus tareas y fortalece tu productividad
+            en el trabajo.
+          </div>
+        </div>
+        <div className="w-auto h-auto md:h-2/4  lg:h-2/4 rounded border  flex flex-col items-center shadow-lg bg-blue-400">
+          <div>
+            <img className="w-40 h-40 " src={image.img5} alt="" />
+          </div>
+          <div className="block text-center text-white font-semibold text-lg">
+            ✅ Agenda tus  citas, entregas o reuniones de tu trabajo sin perder detalles.
+          </div>
+        </div>
+
+      </section>
+    </div>
+  );
+}
+
+export default ButtonReport;
---
+++ src/pages/DashUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ButtonReport from '../components/ButtonReport/ButtonReport';
+import NavbarUser from '../components/NavbarUser/NavbarUser';
+
+function DashUser() {
+  return (
+    <>
+      <NavbarUser />
+      <ButtonReport />
+    </>
+  );
+}
+
+export default DashUser;
