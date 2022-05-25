5ab0071422c2baed9302d0731bf09fa4094cddbf
DiegoBelPe <dbeltranpereira@gmail.com>
2022-05-25 00:04:05 -0500

    install Flowbite

---
+++ src/components/LayoutContainerForm/LayoutContainerForm.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+function LayoutContainerForm() {
+  return (
+    <>
+      layout
+      <Outlet />
+    </>
+  );
+}
+
+export default LayoutContainerForm;
