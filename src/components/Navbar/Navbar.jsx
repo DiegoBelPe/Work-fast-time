3a2182393d1b3779b797ed796f9a04b695392e7e
DiegoBelPe <dbeltranpereira@gmail.com>
2022-05-26 00:38:45 -0500

    feature: style logo-home ok

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
     <div className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
         <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
-          <img className="hidden lg:block h-12 w-auto" src={images.img10} alt="" />
+          <img className="lg:block h-12 w-auto" src={images.img10} alt="" />
         </div>
         <button
           type="button"
